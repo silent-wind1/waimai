456df95d281712dd8903d7b999ac28c096497140
lengyingmofeng <1797719651@qq.com>
2022-10-11 22:35:57 +0800

    Initial commit

---
+++ src/main/java/com/yefeng/XiaofangwaimaiApplication.java
@@ -0,0 +1,13 @@
+package com.yefeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaofangwaimaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaofangwaimaiApplication.class, args);
+    }
+
+}
